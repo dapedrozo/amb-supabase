f6cbbd2f3e0db5846383555836662683727824c6
dapedrozo <lanceroexperto@gmail.com>
2022-12-02 07:11:16 -0500

    tercer commit

--- amb-supabase/src/components/tramites/VerTramitesPersona/VerTramitesMain.jsx
+++ amb-supabase/src/components/tramites/VerTramitesPersona/VerTramitesMain.jsx
@@ -219,7 +219,7 @@ function VerTramitesMain() {
             </div>
           </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
           </div>
         </div>
       </div>
